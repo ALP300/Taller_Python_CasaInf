e1661b36194d1bdc23078d8112e4a8eed7c7850f
ALP300 <aitorpalomino1@gmail.com>
2024-07-20 16:32:36 -0500

    Se realizo el ejercicio 7

---
+++ ejer7.py
@@ -0,0 +1,14 @@
+#Escribir un programa que pida al usuario un número entero 
+# y muestre por pantalla un triángulo rectángulo
+# como el de más abajo, 
+# de altura el número introducido.
+
+#*
+#**
+#***
+#****
+#*****
+
+altura= int(input("Introduce el número entero: "))
+for i in range(1,altura+1):
+    print('*'*i)
